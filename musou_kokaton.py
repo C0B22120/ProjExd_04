9b4a26dce0546c1856d9d59bbc26340c1d110e6a
c0a22010 <c0a220106d@edu.teu.ac.jp>
2023-05-16 17:14:41 +0900

    追加機能１:加速　実装完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -182,7 +182,7 @@ class Explosion(pg.sprite.Sprite):
         引数2 life：爆発時間
         """
         super().__init__()
-        img = pg.image.load("fig/explosion.gif")
+        img = pg.image.load("ex04/fig/explosion.gif")
         self.imgs = [img, pg.transform.flip(img, 1, 1)]
         self.image = self.imgs[0]
         self.rect = self.image.get_rect(center=obj.rect.center)
